553230dd121829ced53e42c8ed61857f64d4df69
newps-dots <raghavharsh214@gmail.com>
2025-08-16 00:47:27 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,12 @@
+a = int(input("enter the value of first number: "))
+b = int(input("enter the value of second number: "))
+
+add = a+b
+sub = a-b
+multi = a*b
+div = a/b
+
+print("Addition: ",add)
+print("subtraction: ",sub)
+print("Multiplication: ",multi)
+print("Division: ",div)
---
+++ assignment2.py
@@ -0,0 +1,3 @@
+first = str(input("enter your first name: "))
+sec = str(input("enter your last name: "))
+print("Hello, ",first, sec,"! welcome to the Python program")
