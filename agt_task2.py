0effb079a2e5007c2ecb4754a3cd7aa95680b9f7
newps-dots <raghavharsh214@gmail.com>
2025-08-20 00:09:21 +0530

    Add files via upload

---
+++ agt_task2.py
@@ -0,0 +1,8 @@
+import math
+a = int(input("enter the a number: "))
+sq = math.sqrt(a)
+lg = math.log(a)
+sine = math.sin(a)
+print("square root: ",sq)
+print("logarithm: ",lg)
+print("sine: ",sine)
