1acc9fc73fe2056251fd1b44d8b11bd94c0e9905
newps-dots <raghavharsh214@gmail.com>
2025-08-24 23:34:27 +0530

    Delete agt_task1.py

--- agt_task1.py
+++
@@ -1,8 +0,0 @@
-a = int(input("enter the value to find its factorial: "))
-def factorial(n):
-    if n < 2:
-        return 1
-    else:
-       return n * (factorial(n-1))
-result = factorial(a)
-print(result)
