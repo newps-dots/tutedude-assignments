da9a6104ab7797372cc708b886b1cbaea244621a
newps-dots <raghavharsh214@gmail.com>
2025-08-27 00:33:40 +0530

    Add files via upload

---
+++ task1_asg5.py
@@ -0,0 +1,3 @@
+dict = {'harsh':'98%','yash':'64%','dhruv':'78%','nishant':'10%'}
+x = input('enter the students name: ')
+print(x,'marks are: ',dict.get(x))
---
+++ task2_asg5.py
@@ -0,0 +1,6 @@
+list = [1,2,3,4,5,6,7,8,9,10]
+x = list[:5]
+print("The original list is:",list)
+print("The sliced list is:",x)
+y = list[:5][::-1]
+print("the reversed sliced list is:",y)
